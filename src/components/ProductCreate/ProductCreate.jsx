ef9b9b445bae0f094935063aeb08108ebc69b3c3
cesarmolina13 <deiby0798@gmail.com>
2023-03-17 10:48:13 -0500

    creacíon base formulario

--- src/components/ProductCreate/ProductCreate.jsx
+++ src/components/ProductCreate/ProductCreate.jsx
@@ -2,8 +2,56 @@ import React from "react";
 
 const ProductCreate = () => {
   return (
+    
     <div>
-      <h1>ProductCreate</h1>
+        <h1>CREATE NEW PRODUCT</h1>
+      <form>
+        <div>
+          <label>Name Product:</label>
+          <input
+          type= "text"
+          name="name_Product"
+          placeholder="Name Product"
+          />
+        </div>
+        <div>
+          <label>Price:</label>
+          <input
+          type="text"
+          name="price"
+          placeholder="Price"
+          />
+        </div>
+        <div>
+          <label>Description:</label>
+          <input
+          type="text"
+          name="description"
+          placeholder="Description"
+          />
+        </div>
+        <div>
+          <label>Size:</label>
+          <input
+          type="text"
+          name="size"
+          placeholder="Size"
+          />
+        </div>
+        <div>
+          <label>image:</label>
+          <input
+          type="text"
+          name="image"
+          placeholder="Url of image"
+          />
+        </div>
+
+        <input
+        type="submit"
+        value= "Create Product"
+        />
+      </form>
     </div>
   );
 };
