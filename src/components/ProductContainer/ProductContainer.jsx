89ab37d908b99494a89ec94efafe27589f3b98bb
Brunex7 <brunoromero200@gmail.com>
2023-03-18 15:24:54 -0300

    Grilla de CardContainer

--- src/components/ProductContainer/ProductContainer.jsx
+++ src/components/ProductContainer/ProductContainer.jsx
@@ -7,7 +7,14 @@ import Product from "../Product/Product";
 const ProductContainer = () => {
    console.log(products)
   const  renderProducts = products?.map(product => (
-    <Grid item key={product.id} display='flex' flexDirection={'column'}
+    <Grid 
+      item 
+      key={product.id} 
+      xs={2}
+      sm={4}
+      md={4}
+      display='flex' 
+      flexDirection={'column'}
       alignItems='center'>
         <Product product = {product}/>
     </Grid>
@@ -17,9 +24,11 @@ const ProductContainer = () => {
   return (
     <Container>
       <Grid 
-        contairner 
+        contairner
+        spacing={{ xs:2, md: 3}}
         justifyContent={'center'} 
         sx={{margin: '20px 4px 10px 4px' }} 
+        columns={{xs: 4, sm: 8, md: 12}}
       >
         {renderProducts}
       </Grid>
