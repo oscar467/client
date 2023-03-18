1ec1d2d3cf96f4ad4cbce292c09cc9a9d780fc20
jandres-vega <jandresvega@itsa.edu.co>
2023-03-17 19:30:02 -0500

    Feact: add url base back

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import axios from 'axios';
 import {Provider}from 'react-redux';
 import {store}from './redux/store';
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+axios.defaults.baseURL = 'https://api-production-bb5a.up.railway.app';
 root.render(
   <React.StrictMode>
       <Provider store={store}>
