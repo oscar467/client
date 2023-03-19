900fdf5b1293f2ee3a1ccd4aea4e52ca4a6c953a
oscar bejarano <osc4r.yt@gmail.com>
2023-03-18 20:16:56 -0500

    correccion containerproducts

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,37 +1,45 @@
-import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
-const Product = ({product}) =>{
+const Product = ({ product }) => {
+  return (
+    <Card sx={{ maxWidth: "250" }}>
+      <CardActionArea sx={{ maxWidth: "250" }}>
+        <CardMedia
+          component="img"
+          alt="placeholder"
+          image={product.image}
+          sx={{ width: 250, maxHeight: 400, margin: 0 }}
+        />
+        <CardContent sx={{ maxWidth: "250" }}>
+          <Typography gutterBottom variant="h5" component="div">
+            {product.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {product.price}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions sx={{ maxWidth: "250" }}>
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<AddShoppingCartIcon />}
+          size="medium"
+        >
+          Add To Cart
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
 
-
-    return (
-        <Card sx={{ maxWidth: 250 }}>
-            <CardActionArea>
-
-            <CardMedia
-                component="img"
-                alt='placeholder'
-                image={product.image}
-                sx={{ width: 250, maxHeight: 400}}
-                />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {product.name}
-                </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {product.price}
-        </Typography>
-      </CardContent>
-        </CardActionArea>
-      <CardActions>
-        <Button 
-        variant="contained"
-        color="primary"
-        startIcon={<AddShoppingCartIcon/>}
-        size="medium">Add To Cart</Button>
-        </CardActions>
-     </Card>
-    )
-}
-
-export default Product
+export default Product;
--- src/components/ProductContainer/ProductContainer.jsx
+++ src/components/ProductContainer/ProductContainer.jsx
@@ -1,40 +1,39 @@
-import { Grid } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
 import { Container } from "@mui/system";
 import React from "react";
-import {products} from '../../data/index';
+import { products } from "../../data/index";
 import Product from "../Product/Product";
 
 const ProductContainer = () => {
-   console.log(products)
-  const  renderProducts = products?.map(product => (
-    <Grid 
-      item 
-      key={product.id} 
+  console.log(products);
+  const renderProducts = products?.map((product) => (
+    <Grid
+      key={product.id}
       xs={2}
       sm={4}
       md={4}
-      display='flex' 
-      flexDirection={'column'}
-      alignItems='center'>
-        <Product product = {product}/>
+      lg={3}
+      display="flex"
+      flexDirection={"column"}
+      alignItems="center"
+    >
+      <Product product={product} />
     </Grid>
   ));
-  
 
   return (
     <Container>
-      <Grid 
-        contairner
-        spacing={{ xs:2, md: 3}}
-        justifyContent={'center'} 
-        sx={{margin: '20px 4px 10px 4px' }} 
-        columns={{xs: 4, sm: 8, md: 12}}
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        justifyContent={"center"}
+        columns={{ xs: 4, sm: 8, md: 12, lg: 12 }}
+        sx={{ margin: "20px 4px 10px 4px" }}
       >
         {renderProducts}
       </Grid>
       {/* <Product/> */}
     </Container>
-
   );
 };
 
