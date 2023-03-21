6a96f26ef6790658b3b0c6de46a2811bf64ee983
cesarmolina13 <deiby0798@gmail.com>
2023-03-21 00:18:14 -0500

    creacion action post

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,3 +1,9 @@
 export const typesCategories = {
     getAlCategories: 'GET_ALL_CATEGORIES'
 }
+
+
+    export const typesProducts= {
+        createProduct: 'POST_PRODUCT',
+        getAllProducts: 'GET_ALL_PRODUCTS'
+    }
