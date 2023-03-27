e325cde8baeb6fab0233115a6dff4a367c13f588
jandres-vega <jandresvega@itsa.edu.co>
2023-03-27 07:56:29 -0500

    Feact: modified ruter

--- src/App.js
+++ src/App.js
@@ -17,43 +17,47 @@ import Contactanos from "./components/Footer/contactanos";
 import Detail from "./components/Detail/Detail";
 import ContainerFormRegisterUser from "./components/RegisterUser/ContainerFormRegisterUser";
 import ContainerLogin from "./components/FormLogin/ContainerLogin";
+import {currentUser} from './redux/actions/userActions';
+import {useDispatch} from "react-redux";
+import DashboardMain from "./components/Dashboard/DashboardMain";
+import Layout from "./components/Layout/Layout";
+import LayoutDashBoard from "./components/Layout/LayoutDashBoard";
 
 function App() {
+    const dispatch = useDispatch();
+    React.useEffect(() => {
+        const dataUser = localStorage.getItem('auth');
+        if (dataUser){
+            dispatch(currentUser(JSON.parse(dataUser)))
+        }
+    },[dispatch])
   return (
-    <div>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/ProductCatalog" element={<ProductCatalog />} />
-        <Route
-          path="/ProductCatalog/category/:category"
-          element={<ProductCatalog />}
-        />
-        <Route path="/ProductCatalog/product/:id" element={<Detail />} />
-        <Route path="/ProductCreate" element={<ProductCreate />} />
-        <Route path="/Contact" element={<Contact />} />
-      </Routes>
-
-      <Footer />
-      <Routes>
-        <Route path="/Tienda" element={<Tienda />} />
-        <Route path="/Promociones" element={<Promociones />} />
-        <Route
-          path="/TerminosyCondiciones"
-          element={<TerminosyCondiciones />}
-        />
-        <Route
-          path="/CambiosyDevoluciones"
-          element={<CambiosyDevoluciones />}
-        />
-        <Route path="/Confidencialidad" element={<Confidencialidad />} />
-        <Route path="/Envios" element={<Envios />} />
-        <Route path="/PreguntasFrecuentes" element={<PreguntasFrecuentes />} />
-        <Route path="/Contáctanos" element={<Contactanos />} />
-          <Route path="/register-user" element={<ContainerFormRegisterUser/>} />
-          <Route path="/login" element={<ContainerLogin/>} />
-      </Routes>
-    </div>
+      <React.Fragment>
+          <Routes>
+              <Route path="/" element={<Layout />}>
+                  <Route path="/Confidencialidad" element={<Confidencialidad />} />
+                  <Route path="/Envios" element={<Envios />} />
+                  <Route path="/PreguntasFrecuentes" element={<PreguntasFrecuentes />} />
+                  <Route path="/Contáctanos" element={<Contactanos />} />
+                  <Route path="/register-user" element={<ContainerFormRegisterUser/>} />
+                  <Route path="/login" element={<ContainerLogin/>} />
+                  <Route path="/Tienda" element={<Tienda />} />
+                  <Route path="/Promociones" element={<Promociones />} />
+                  <Route exact path="/" element={<Home />} />
+                  <Route path="/ProductCatalog" element={<ProductCatalog />} />
+                  <Route path="/ProductCatalog/category/:category" element={<ProductCatalog />}/>
+                  <Route path="/ProductCatalog/product/:id" element={<Detail />} />
+                  <Route path="/Contact" element={<Contact />} />
+                  <Route path="/TerminosyCondiciones" element={<TerminosyCondiciones />}/>
+                  <Route path="/CambiosyDevoluciones" element={<CambiosyDevoluciones />}/>
+              </Route>
+          </Routes>
+          <Routes>
+              <Route path="dashboard-admin" element={<LayoutDashBoard />} >
+                  <Route path="product-create" element={<ProductCreate />} />
+              </Route>
+          </Routes>
+      </React.Fragment>
   );
 }
 
