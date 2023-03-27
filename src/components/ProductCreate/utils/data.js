d40286c35cd44c1512a686dce4984d21cb6b5c60
jandres-vega <jandresvega@itsa.edu.co>
2023-03-27 07:49:24 -0500

    Feact: add hooksand utils for controller form register product

---
+++ src/components/ProductCreate/utils/data.js
@@ -0,0 +1,12 @@
+export const sizes=["S", "M", "L", "XL"];
+
+export const initialForm = {
+    name_product:"",
+    image:"",
+    price:"",
+    description:"",
+    size:[],
+    stock:"",
+    discount:"",
+    CategoryId:""
+}
