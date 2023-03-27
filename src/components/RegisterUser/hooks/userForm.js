07bae8246c165e928d7d13270cbcd80c3c084897
jandres-vega <jandresvega@itsa.edu.co>
2023-03-27 07:52:21 -0500

    Feact: add validation and hooks to form register  user

--- src/components/RegisterUser/hooks/userForm.js
+++ src/components/RegisterUser/hooks/userForm.js
@@ -1,28 +1,45 @@
 import {createUser} from '../../../redux/actions/userActions';
+import React from "react";
+import Swal from 'sweetalert2'
 import {useDispatch} from "react-redux";
 import {useNavigate} from "react-router-dom";
+
 
 export const useForm = (form) => {
     const dispatch = useDispatch();
     const navigate = useNavigate()
+    const [errors, setErrors] = React.useState({})
     const handleSubmit = (e) => {
         e.preventDefault();
+        let errors = {}
         const formData = new FormData(form.current);
-
-        const user = {
-            fullName: formData.get('fullName'),
-            email: formData.get('email'),
-            password: formData.get('password'),
-            cellPhone: formData.get('phone'),
+        if (!formData.get('fullName').trim()) errors.fullName = "Ingrese el nombre completo";
+        if (!formData.get('email').trim()) errors.email = "Ingrese el correo electronico";
+        if (!formData.get('password').trim()) errors.pass = "Ingrese una contraseña";
+        if (!formData.get('phone').trim()) errors.phone = "Ingrese un telefono";
+        if (Object.keys(errors).length === 0){
+            const user = {
+                fullName: formData.get('fullName'),
+                email: formData.get('email'),
+                password: formData.get('password'),
+                cellPhone: formData.get('phone'),
+            }
+            dispatch(createUser(user)).then(() => {
+                navigate('/login')
+            }).catch(() => {
+                Swal.fire(
+                    'ERROR',
+                    "Ingresaste mal los datos",
+                    'error'
+                )
+            })
+        }else {
+            setErrors(errors)
         }
-        dispatch(createUser(user)).then(() => {
-            navigate('/login')
-        }).catch(err => {
-            alert(err.response.data.message)
-        })
     }
 
     return {
-        handleSubmit
+        handleSubmit,
+        errors
     }
 }
