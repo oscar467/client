5b801a19aa56eabc74ff42b2725186e2921d7b2f
jandres-vega <jandresvega@itsa.edu.co>
2023-03-28 17:16:04 -0500

    Feact: Fix routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
-import Footer from "./components/Footer/Footer";
 import ProductCatalog from "./components/ProductCatalog/ProductCatalog";
 import ProductCreate from "./components/ProductCreate/ProductCreate";
 import Contact from "./components/Contact/Contact";
@@ -11,8 +11,6 @@ import CambiosyDevoluciones from "./components/Footer/cambiosydevoluciones";
 import Confidencialidad from "./components/Footer/confidencialidad";
 import Envios from "./components/Footer/envios";
 import PreguntasFrecuentes from "./components/Footer/preguntasfrecuentes";
-import MetodosdePago from "./components/Footer/metodosdepago";
-import QuienesSomos from "./components/Footer/quienessomos";
 import Contactanos from "./components/Footer/contactanos";
 import Detail from "./components/Detail/Detail";
 import ContainerFormRegisterUser from "./components/RegisterUser/ContainerFormRegisterUser";
@@ -21,7 +19,8 @@ import {currentUser} from './redux/actions/userActions';
 import {useDispatch} from "react-redux";
 import Layout from "./components/Layout/Layout";
 import LayoutDashBoard from "./components/Layout/LayoutDashBoard";
-import NavBar from "./components/NavBar/NavBar";
+
+
 
 function App() {
     const dispatch = useDispatch();
@@ -31,37 +30,34 @@ function App() {
             dispatch(currentUser(JSON.parse(dataUser)))
         }
     },[dispatch])
-  return (
-    <div>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/ProductCatalog" element={<ProductCatalog />} />
-        <Route
-          path="/ProductCatalog/category/:category"
-          element={<ProductCatalog />}
-        />
-        <Route path="/ProductCatalog/product/:id" element={<Detail />} />
-        <Route path="/ProductCreate" element={<ProductCreate />} />
-        <Route path="/Contact" element={<Contact />} />
-      </Routes>
-
-      <Footer />
-      <Routes>
-        <Route path="/QuienesSomos" element={<QuienesSomos />} />
-        <Route path="/Promociones" element={<Promociones />} />
-        <Route path="/MetodosdePago" element={<MetodosdePago />} />
-        <Route path="/TerminosyCondiciones"element={<TerminosyCondiciones />}/>
-        <Route path="/CambiosyDevoluciones"element={<CambiosyDevoluciones />}/>
-        <Route path="/Confidencialidad" element={<Confidencialidad />} />
-        <Route path="/Envios" element={<Envios />} />
-        <Route path="/PreguntasFrecuentes" element={<PreguntasFrecuentes />} />
-        <Route path="/Contactanos" element={<Contactanos />} />
-        <Route path="/register-user" element={<ContainerFormRegisterUser/>} />
-        <Route path="/login" element={<ContainerLogin/>} />
-      </Routes>
-    </div>
-  );
+    return (
+        <React.Fragment>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="/Confidencialidad" element={<Confidencialidad />} />
+                    <Route path="/Envios" element={<Envios />} />
+                    <Route path="/PreguntasFrecuentes" element={<PreguntasFrecuentes />} />
+                    <Route path="/Contáctanos" element={<Contactanos />} />
+                    <Route path="/register-user" element={<ContainerFormRegisterUser/>} />
+                    <Route path="/login" element={<ContainerLogin/>} />
+                    <Route path="/Promociones" element={<Promociones />} />
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/ProductCatalog" element={<ProductCatalog />} />
+                    <Route path="/ProductCatalog/category/:category" element={<ProductCatalog />}/>
+                    <Route path="/ProductCatalog/product/:id" element={<Detail />} />
+                    <Route path="/Contact" element={<Contact />} />
+                    <Route path="/TerminosyCondiciones" element={<TerminosyCondiciones />}/>
+                    <Route path="/CambiosyDevoluciones" element={<CambiosyDevoluciones />}/>
+                </Route>
+            </Routes>
+            <Routes>
+                <Route path="dashboard-admin" element={<LayoutDashBoard />} >
+                    <Route path="product-create" element={<ProductCreate />} />
+                </Route>
+            </Routes>
+        </React.Fragment>
+    );
 }
 
 export default App;
+
